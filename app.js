db6db39996f53052bca5e201e839a43087fd1462
Hongjia-Liu <hongjia_liu@outlook.com>
2024-08-04 08:19:04 +0800

    feat: init express server with custom 404 response

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Node Store API");
+const express = require("express");
+const app = express();
+const { notFount } = require("./middleware/not-found");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.use(notFount);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
